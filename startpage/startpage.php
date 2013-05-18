2342243ef9a49410713380beb1c011aee304b499
friendica <info@friendica.com>
2013-05-18 01:43:11 -0700

    update startpage to new home page preferences

--- startpage/startpage.php
+++ startpage/startpage.php
@@ -9,7 +9,7 @@
 
 
 function startpage_load() {
-	register_hook('home_init', 'addon/startpage/startpage.php', 'startpage_home_init');
+//	register_hook('home_init', 'addon/startpage/startpage.php', 'startpage_home_init');
 	register_hook('feature_settings', 'addon/startpage/startpage.php', 'startpage_settings');
 	register_hook('feature_settings_post', 'addon/startpage/startpage.php', 'startpage_settings_post');
 }
@@ -27,8 +27,8 @@ function startpage_home_init($a, $b) {
 	if(! local_user())
 		return;
 
-	$page = get_pconfig(local_user(),'startpage','startpage');
-	if(strlen($page)) {
+	$page = get_pconfig(local_user(),'system','startpage');
+	if(strlen($page)) {		
 		$slash = ((strpos($page,'/') === 0) ? true : false);
 		if(stristr($page,'://'))
 			goaway($page);
@@ -49,8 +49,21 @@ function startpage_home_init($a, $b) {
 function startpage_settings_post($a,$post) {
 	if(! local_user())
 		return;
-	if($_POST['startpage-submit'])
-		set_pconfig(local_user(),'startpage','startpage',strip_tags(trim($_POST['startpage'])));
+	$c = $a->get_channel();
+
+	if($_POST['startpage-submit']) {
+		$page = strip_tags(trim($_POST['startpage']));
+		$page = trim($page,'/');
+
+		if($page == 'channel')
+			$page = z_root() .'/channel/' . $channel['channel_address'];
+		elseif($page == '')
+			$page = '';
+		else
+			if($strpos($page,'http') !== 0)
+				$page = z_root() . '/' . $page;
+
+		set_pconfig(local_user(),'system','startpage',$page);
 }
 
 
@@ -74,7 +87,7 @@ function startpage_settings(&$a,&$s) {
 
 	/* Get the current state of our config variable */
 
-	$page = get_pconfig(local_user(),'startpage','startpage');
+	$page = get_pconfig(local_user(),'system','startpage');
 
 
 	/* Add some HTML to the existing form */
@@ -82,10 +95,10 @@ function startpage_settings(&$a,&$s) {
 	$s .= '<div class="settings-block">';
 	$s .= '<h3>' . t('Startpage Settings') . '</h3>';
 	$s .= '<div id="startpage-page-wrapper">';
-	$s .= '<label id="startpage-page-label" for="startpage-page">' . t('Home page to load after login  - leave blank for channel home') . '</label>';
+	$s .= '<label id="startpage-page-label" for="startpage-page">' . t('Home page to load after login  - leave blank for matrix page') . '</label>';
 	$s .= '<input id="startpage-page" type="text" name="startpage" value="' . $page . '" />';
 	$s .= '</div><div class="clear"></div>';
-	$s .= '<div id="startpage-desc">' . t('Examples: &quot;network&quot; or &quot;notifications/system&quot;') . '</div>';
+	$s .= '<div id="startpage-desc">' . t('Examples: &quot;channel&quot; or &quot;notifications/system&quot;') . '</div>';
 
 	/* provide a submit button */
 

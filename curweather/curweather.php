394626037993567dab39aa611e1414295dfe14a1
Tazman DeVille <tazmandevil@gmx.com>
2013-09-15 14:32:42 +0200

    right or left aside option for curweather

--- curweather/curweather.php
+++ curweather/curweather.php
@@ -10,6 +10,7 @@ require_once('addon/curweather/getweather.php');
 
 function curweather_load() {
 	register_hook('network_mod_init', 'addon/curweather/curweather.php', 'curweather_network_mod_init');
+	register_hook('channel_mod_init', 'addon/curweather/curweather.php', 'curweather_channel_mod_init');
 	register_hook('feature_settings', 'addon/curweather/curweather.php', 'curweather_settings');
 	register_hook('feature_settings_post', 'addon/curweather/curweather.php', 'curweather_settings_post');
 
@@ -17,13 +18,14 @@ function curweather_load() {
 
 function curweather_unload() {
 	unregister_hook('network_mod_init', 'addon/curweather/curweather.php', 'curweather_network_mod_init');
+	unregister_hook('channel_mod_init', 'addon/curweather/curweather.php', 'curweather_channel_mod_init');
 	unregister_hook('feature_settings', 'addon/curweather/curweather.php', 'curweather__settings');
 	unregister_hook('feature_settings_post', 'addon/curweather/curweather.php', 'curweather_settings_post');
 
 }
 
 
-function curweather_network_mod_init(&$a,&$b) {
+function curweather_channel_mod_init(&$a,&$b) {
 
     if(! intval(get_pconfig(local_user(),'curweather','curweather_enable')))
         return;
@@ -57,11 +59,51 @@ function curweather_network_mod_init(&$a,&$b) {
 }
 
 
+function curweather_network_mod_init(&$a,&$b) {
+
+    if(! intval(get_pconfig(local_user(),'curweather','curweather_enable')))
+        return;
+
+    $a->page['htmlhead'] .= '<link rel="stylesheet"  type="text/css" href="' . $a->get_baseurl() . '/addon/curweather/curweather.css' . '" media="all" />' . "\r\n";
+
+    // the getweather file does all the work here
+    // the $rpt value is needed for location
+    // which getweather uses to fetch the weather data for weather and temp
+    $rpt = get_pconfig(local_user(), 'curweather', 'curweather_loc');
+    $wxdata = GetWeather::get($rpt);
+    $temp = $wxdata['TEMPERATURE_STRING'];
+    $weather = $wxdata['WEATHER'];
+    $rhumid = $wxdata['RELATIVE_HUMIDITY'];
+    $pressure = $wxdata['PRESSURE_STRING'];
+    $wind = $wxdata['WIND_STRING'];
+    $curweather = '<div id="curweather-network" class="widget">
+                <div class="title tool">
+                <h4>'.t("Current Weather").'</h4></div>';
+
+    $curweather .= "Weather: $weather<br />
+                 Temperature: $temp<br />
+		 Relative Humidity: $rhumid<br />
+		 Pressure: $pressure<br />
+		 Wind: $wind";
+
+    $curweather .= '</div><div class="clear"></div>';
+
+
+    if (! intval(get_pconfig(local_user(), 'curweather', 'curweather_right'))) {
+	    $a->page['aside'] = $curweather.$a->page['aside'];
+    }	else {
+	    $a->page['right_aside'] = $curweather.$a->page['right_aside'];
+    }
+
+}
+
+
 function curweather_settings_post($a,$s) {
 	if(! local_user() || (! x($_POST,'curweather-settings-submit')))
 		return;
 	set_pconfig(local_user(),'curweather','curweather_loc',trim($_POST['curweather_loc']));
 	set_pconfig(local_user(),'curweather','curweather_enable',intval($_POST['curweather_enable']));
+	set_pconfig(local_user(),'curweather','curweather_right',intval($_POST['curweather_right']));
 
 	info( t('Current Weather settings updated.') . EOL);
 }
@@ -79,6 +121,8 @@ function curweather_settings(&$a,&$s) {
 	/* Get the current state of our config variable */
 
 	$curweather_loc = get_pconfig(local_user(), 'curweather', 'curweather_loc');
+	$right = intval(get_pconfig(local_user(),'curweather','curweather_right'));
+	$right_checked = (($right) ? ' checked="checked" ' : '');
 	$enable = intval(get_pconfig(local_user(),'curweather','curweather_enable'));
 	$enable_checked = (($enable) ? ' checked="checked" ' : '');
 	
@@ -92,7 +136,10 @@ function curweather_settings(&$a,&$s) {
 	$s .= '<label id="curweather-location-label" for="curweather_loc">' . t('Weather Location: ') . '</label>';
 	$s .= '<input id="curweather-location" type="text" name="curweather_loc" value="' . $curweather_loc . '"/>';
 	$s .= '<div class="clear"></div>';
-	$s .= '<label id="curweather-enable-label" for="curweather_enable">' . t('Enable Current Weather') . '</label>';
+	$s .= '<label id="curweather-right-label" for="curweather_right">' . t('Right side (left defaul)') . '</label><br />';
+	$s .= '<input id="curweather-right" type="checkbox" name="curweather_right" value="1" ' . $right_checked . '/>';
+	$s .= '<div class="clear"></div>';
+	$s .= '<label id="curweather-enable-label" for="curweather_enable">' . t('Enable Current Weather') . '</label><br />';
 	$s .= '<input id="curweather-enable" type="checkbox" name="curweather_enable" value="1" ' . $enable_checked . '/>';
 	$s .= '<div class="clear"></div>';
 

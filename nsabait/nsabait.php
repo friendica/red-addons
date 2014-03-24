a0322729c5aad02854d030012f9201f45d2639c0
friendica <info@friendica.com>
2014-03-23 22:00:04 -0700

    add nsabait - make yourself a political target

---
+++ nsabait/nsabait.php
@@ -0,0 +1,165 @@
+<?php
+/**
+ * Name: NSA bait
+ * Description: Make yourself a political target
+ * Version: 1.0
+ * Author: Mike Macgirvin <http://macgirvin.com/profile/mike>
+ *
+ */
+
+
+function nsabait_load() {
+
+	/**
+	 * 
+	 * Our demo plugin will attach in three places.
+	 * The first is just prior to storing a local post.
+	 *
+	 */
+
+	register_hook('post_local_start', 'addon/nsabait/nsabait.php', 'nsabait_post_hook');
+
+	/**
+	 *
+	 * Then we'll attach into the plugin settings page, and also the 
+	 * settings post hook so that we can create and update
+	 * user preferences.
+	 *
+	 */
+
+	register_hook('feature_settings', 'addon/nsabait/nsabait.php', 'nsabait_settings');
+	register_hook('feature_settings_post', 'addon/nsabait/nsabait.php', 'nsabait_settings_post');
+
+	logger("loaded nsabait");
+}
+
+
+function nsabait_unload() {
+
+	/**
+	 *
+	 * unload unregisters any hooks created with register_hook
+	 * during load. It may also delete configuration settings
+	 * and any other cleanup.
+	 *
+	 */
+
+	unregister_hook('post_local_start',    'addon/nsabait/nsabait.php', 'nsabait_post_hook');
+	unregister_hook('feature_settings', 'addon/nsabait/nsabait.php', 'nsabait_settings');
+	unregister_hook('feature_settings_post', 'addon/nsabait/nsabait.php', 'nsabait_settings_post');
+
+
+	logger("removed nsabait");
+}
+
+
+
+function nsabait_post_hook($a, &$item) {
+	/**
+	 *
+	 * An item was posted on the local system.
+	 * We are going to look for specific items:
+	 *      - A status post by a profile owner
+	 *      - The profile owner must have allowed our plugin
+	 *
+	 */
+
+	logger('nsabait invoked');
+
+	if(! local_user())   /* non-zero if this is a logged in user of this system */
+		return;
+
+	if(local_user() != $item['profile_uid'])    /* Does this person own the post? */
+		return;
+
+	if($item['parent'])   /* If the item has a parent, this is a comment or something else, not a status post. */
+		return;
+
+	/* Retrieve our personal config setting */
+
+	$active = get_pconfig(local_user(), 'nsabait', 'enable');
+
+	if(! $active)
+		return;
+
+	$words = file_get_contents('addon/nsabait/wordlist.txt');
+	$nsabait = explode(',',$words);
+	shuffle($nsabait);
+	$used = array();
+
+	$item['body'] .= "\n";
+
+	for($x = 0; $x < 5; $x ++) {
+		$y = mt_rand(0,count($nsabait));
+		if((in_array(strtolower(trim($nsabait[$y])),$used)) || (! trim($nsabait[$y]))) {
+			$x -= 1;
+			continue;
+		}
+		$used[] = strtolower(trim($nsabait[$y]));
+
+		$item['body'] .= ' #' . str_replace(' ','_',trim($nsabait[$y]));
+	}
+
+	return;
+}
+
+
+
+
+/**
+ *
+ * Callback from the settings post function.
+ * $post contains the $_POST array.
+ * We will make sure we've got a valid user account
+ * and if so set our configuration setting for this person.
+ *
+ */
+
+function nsabait_settings_post($a,$post) {
+	if(! local_user())
+		return;
+	if($_POST['nsabait-submit']) {
+		set_pconfig(local_user(),'nsabait','enable',intval($_POST['nsabait']));
+		info( t('Nsabait Settings updated.') . EOL);
+	}
+}
+
+
+/**
+ *
+ * Called from the Plugin Setting form. 
+ * Add our own settings info to the page.
+ *
+ */
+
+
+
+function nsabait_settings(&$a,&$s) {
+
+	if(! local_user())
+		return;
+
+	/* Add our stylesheet to the page so we can make our settings look nice */
+
+	$a->page['htmlhead'] .= '<link rel="stylesheet"  type="text/css" href="' . $a->get_baseurl() . '/addon/nsabait/nsabait.css' . '" media="all" />' . "\r\n";
+
+	/* Get the current state of our config variable */
+
+	$enabled = get_pconfig(local_user(),'nsabait','enable');
+
+	$checked = (($enabled) ? ' checked="checked" ' : '');
+
+	/* Add some HTML to the existing form */
+
+	$s .= '<div class="settings-block">';
+	$s .= '<h3>' . t('NSAbait Settings') . '</h3>';
+	$s .= '<div id="nsabait-enable-wrapper">';
+	$s .= '<label id="nsabait-enable-label" for="nsabait-checkbox">' . t('Enable NSAbait Plugin') . '</label>';
+	$s .= '<input id="nsabait-checkbox" type="checkbox" name="nsabait" value="1" ' . $checked . '/>';
+	$s .= '</div><div class="clear"></div>';
+
+	/* provide a submit button */
+
+	$s .= '<div class="settings-submit-wrapper" ><input type="submit" name="nsabait-submit" class="settings-submit" value="' . t('Submit') . '" /></div></div>';
+
+}

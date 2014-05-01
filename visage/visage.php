22b5f441849b94d12cab8cafc6c46cc6f28166e0
friendica <info@friendica.com>
2014-04-30 17:31:32 -0700

    fix visage after magic-auth protocol changes several months ago

--- visage/visage.php
+++ visage/visage.php
@@ -38,8 +38,16 @@ function visage_magic_auth($a, &$b) {
 //		logger('visage: exiting: ' . $b['url']);
 		return;
 	}
+
+	$p = preg_match('/http(.*?)(channel|profile)\/(.*?)($|[\/\?\&])/',$b['url'],$matches);
+	if(! $p) {
+//		logger('visage: no matching pattern');
+		return;
+	}
+
+//	logger('visage: matches ' . print_r($matches,true));
 	
-	$nick = $b['channel_address'];
+	$nick = $matches[3];
 
 	$c = q("select channel_id, channel_hash from channel where channel_address = '%s' limit 1",
 		dbesc($nick)

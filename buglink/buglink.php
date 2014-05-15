67bc42d1ca4adc7047727761da1f13537ce6c96c
friendica <info@friendica.com>
2014-05-14 20:56:42 -0700

    hide buglink on small screens

--- buglink/buglink.php
+++ buglink/buglink.php
@@ -12,4 +12,4 @@ function buglink_load() { register_hook('page_end', 'addon/buglink/buglink.php',
 
 function buglink_unload() { unregister_hook('page_end', 'addon/buglink/buglink.php', 'buglink_active'); }
 
-function buglink_active(&$a,&$b) { $b .= '<div style="position: fixed; bottom: 5px; left: 5px;"><a href="https://github.com/friendica/red/issues" target="_blank" title="' . t('Report Bug') . '"><img src="addon/buglink/bug-x.gif" alt="' . t('Report Bug') . '" /></a></div>'; } 
+function buglink_active(&$a,&$b) { $b .= '<div style="position: fixed; bottom: 5px; left: 5px;" class="hidden-xs"><a href="https://github.com/friendica/red/issues" target="_blank" title="' . t('Report Bug') . '"><img src="addon/buglink/bug-x.gif" alt="' . t('Report Bug') . '" /></a></div>'; } 

bf9032a131cad73b0dce4c904a7cb4a48c038b4c
friendica <info@friendica.com>
2014-06-07 01:23:43 -0700

    flashlight app ;-)

---
+++ torch/torch.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * Name: Torch
+ * Description: Flashlight
+ * Version: 1.0
+ * Author: Macgirvin
+ *
+ */
+
+
+function torch_load() {}
+function torch_unload() {}
+function torch_module() {}
+
+
+function torch_content(&$a) {
+	$a = @$foo();
+
+}

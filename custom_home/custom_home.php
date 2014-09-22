5c2c2f7962c3c2a05b32cb452d933f65df28ae37
Thomas Willingham <beardyunixer@beardyunixer.com>
2014-09-22 05:18:58 +0100

    *sigh* more 5am-itis

--- custom_home/custom_home.php
+++ custom_home/custom_home.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Name: Custom Home
- * Description: Set a custom home page.  Display a random channel from this server on the home page.
+ * Description: Set a custom home page or display a random channel from this server on the home page.
  * Version: 1.0  
  * Author: Thomas Willingham <zot:beardyunixer@beardyunixer.com>
  */
@@ -9,12 +9,12 @@
 
 function custom_home_load() {
     register_hook('home_content', 'addon/custom_home/custom_home.php', 'custom_home_home');
-    logger("loaded random_channel_home");
+    logger("loaded custom_home");
 }
  
 function custom_home_unload() {
     unregister_hook('home_content', 'addon/custom_home/custom_home.php', 'custom_home_home');
-    logger("removed random_channel_home");
+    logger("removed custom_home");
 }
  
 function custom_home_home(&$a, &$o){

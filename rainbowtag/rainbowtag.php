45895aaafe4bca480ed14f8c42d53cf64fc1f5c5
friendica <info@friendica.com>
2015-03-01 23:49:37 -0800

    make colours customisable, even though there is no UI to do this.

--- rainbowtag/rainbowtag.php
+++ rainbowtag/rainbowtag.php
@@ -33,18 +33,31 @@ function rainbowtag_construct_page(&$a,&$b) {
 	if(! intval(get_pconfig($a->profile_uid,'rainbowtag','enable')))
 		return;
 
+	$c = get_pconfig($a->profile_uid,'rainbowtag','colors');
+	$color1 = ((is_array($c) && $c[0]) ? $c[0] : 'LawnGreen');
+	$color2 = ((is_array($c) && $c[1]) ? $c[1] : 'DarkBlue');
+	$color3 = ((is_array($c) && $c[2]) ? $c[2] : 'Red');
+	$color4 = ((is_array($c) && $c[3]) ? $c[3] : 'DarkOrange');
+	$color5 = ((is_array($c) && $c[4]) ? $c[4] : 'Gold');
+	$color6 = ((is_array($c) && $c[5]) ? $c[5] : 'Teal');
+	$color7 = ((is_array($c) && $c[6]) ? $c[6] : 'Sienna');
+	$color8 = ((is_array($c) && $c[7]) ? $c[7] : 'DarkMagenta');
+	$color9 = ((is_array($c) && $c[8]) ? $c[8] : 'GreenYellow');
+	$color10 = ((is_array($c) && $c[9]) ? $c[9] : 'DeepPink');
+
+		
 
 	$o = '<style>';
-	$o .= '.tag1 { color: LawnGreen !important; }' . "\r\n";
-	$o .= '.tag2 { color: DarkBlue !important; }' . "\r\n";
-	$o .= '.tag3 { color: Red !important; }' . "\r\n";
-	$o .= '.tag4 { color: DarkOrange !important; }' . "\r\n";
-	$o .= '.tag5 { color: Gold !important; }' . "\r\n";
-	$o .= '.tag6 { color: Teal !important; }' . "\r\n";
-	$o .= '.tag7 { color: Sienna !important; }' . "\r\n";
-	$o .= '.tag8 { color: DarkMagenta !important; }' . "\r\n";
-	$o .= '.tag9 { color: GreenYellow !important; }' . "\r\n";
-	$o .= '.tag10 { color: DeepPink !important; }' . "\r\n";
+	$o .= '.tag1  { color: ' . $color1  . ' !important; }' . "\r\n";
+	$o .= '.tag2  { color: ' . $color2  . ' !important; }' . "\r\n";
+	$o .= '.tag3  { color: ' . $color3  . ' !important; }' . "\r\n";
+	$o .= '.tag4  { color: ' . $color4  . ' !important; }' . "\r\n";
+	$o .= '.tag5  { color: ' . $color5  . ' !important; }' . "\r\n";
+	$o .= '.tag6  { color: ' . $color6  . ' !important; }' . "\r\n";
+	$o .= '.tag7  { color: ' . $color7  . ' !important; }' . "\r\n";
+	$o .= '.tag8  { color: ' . $color8  . ' !important; }' . "\r\n";
+	$o .= '.tag9  { color: ' . $color9  . ' !important; }' . "\r\n";
+	$o .= '.tag10 { color: ' . $color10 . ' !important; }' . "\r\n";
 	$o .= '</style>';
 
 	$a->page['htmlhead'] .= $o;
